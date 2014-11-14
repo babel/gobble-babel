e913b9cdecb766e2373ecb5f112a3a7a58276fbe
Rich Harris <richard.a.harris@gmail.com>
2014-11-14 17:47:53 -0500

    return code or object with code/map depending on sourceMap option

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ module.exports = _6to5;
 
 function _6to5 ( code, options ) {
 	options.filename = this.filename;
-	//options.sourceMap = true;
-	var transformed = require( '6to5' ).transform( code, options );
 
-	return transformed.code;
+	var transformed = require( '6to5' ).transform( code, options );
+	return options.sourceMap ? transformed : transformed.code;
 }
 
 _6to5.defaults = {
