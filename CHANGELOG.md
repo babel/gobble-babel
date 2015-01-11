75000cb59de06be537484bea58081db1a576c976
Rich Harris <richard.a.harris@gmail.com>
2015-01-11 08:56:14 -0500

    fix stupid bug, bump -> 1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # changelog
 
+## 1.0.2
+
+* Require 6to5-core, not 6to5 (duh)
+
 ## 1.0.1
 
 * Switch to 6to5-core (avoids node-gyp nonsense)
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = _6to5;
 
 function _6to5 ( code, options ) {
 	options.sourceMap = options.sourceMap !== false;
-	return require( '6to5' ).transform( code, options );
+	return require( '6to5-core' ).transform( code, options );
 }
 
 _6to5.defaults = {
