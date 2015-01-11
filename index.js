9f5d08dbaffee57a51a3778cab925fe12e91bd65
Rich Harris <richard.a.harris@gmail.com>
2015-01-11 14:19:54 -0500

    use id option

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-module.exports = _6to5;
+module.exports = to5;
 
-function _6to5 ( code, options ) {
+function to5 ( code, options ) {
 	options.sourceMap = options.sourceMap !== false;
 	return require( '6to5-core' ).transform( code, options );
 }
 
-_6to5.defaults = {
+to5.id = '6to5';
+
+to5.defaults = {
 	accept: '.js'
 };
