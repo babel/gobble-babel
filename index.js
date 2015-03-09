3c48a5f3572c4471c79112a563034ace3027d382
Rich Harris <richard.a.harris@gmail.com>
2015-03-09 12:07:07 -0400

    remove unused id option

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+var transform = require( 'babel-core' ).transform;
+
 module.exports = babel;
 
 function babel ( code, options ) {
 	options.sourceMap = options.sourceMap !== false;
-	return require( 'babel-core' ).transform( code, options );
+	return transform( code, options );
 }
-
-babel.id = 'babel';
 
 babel.defaults = {
 	accept: '.js'
