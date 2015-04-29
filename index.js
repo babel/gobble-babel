b410e3a161f6d91a40de28a4c61888a4361b64e9
Rich Harris <richard.a.harris@gmail.com>
2015-04-29 16:12:17 -0400

    respect .babelrc files - closes #6

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var transform = require( 'babel-core' ).transform;
+var resolveRc = require( 'babel-core/lib/babel/tools/resolve-rc' );
+var dirname = require( 'path' ).dirname;
 
 module.exports = babel;
 
 function babel ( code, options ) {
 	options.sourceMap = options.sourceMap !== false;
+
+	// apply .babelrc files
+	resolveRc( dirname( this.src ), options );
+
 	return transform( code, options );
 }
 
